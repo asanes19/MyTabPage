494bf37eeaa076774b0e810dbe0ecb56a6511f24
unknown <abdelrahmansherif1930@gmail.com>
2023-08-20 22:22:00 +0400

    files

---
+++ assets/js/firebase.js
@@ -0,0 +1,78 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB4StbfPiQrwdq419Zjuj3ZTqMMQfQPAjc",
+  authDomain: "myownhomepage-15bf01930.firebaseapp.com",
+  databaseURL: "https://myownhomepage-15bf01930-default-rtdb.firebaseio.com",
+  projectId: "myownhomepage-15bf01930",
+  storageBucket: "myownhomepage-15bf01930.appspot.com",
+  messagingSenderId: "1062804101168",
+  appId: "1:1062804101168:web:7d4659d8aea216a2cfb5f5"
+};
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// reference your database
+var myOwnHomePageDB = firebase.database().ref("myOwnHomePage");
+
+document.getElementById("linkForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  var linkName = getElementVal("linkName");
+  var linkURL = getElementVal("linkURL");
+  var linkType = getElementVal("linkType");
+
+  saveMessages(linkName, linkURL, linkType);
+
+  location.reload();
+}
+
+const saveMessages = (linkName, linkURL, linkType) => {
+  var newmyOwnHomePage = myOwnHomePageDB.push();
+
+  newmyOwnHomePage.set({
+    linkName: linkName,
+    linkURL: linkURL,
+    linkType: linkType,
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
+
+myOwnHomePageDB.once("value").then((snapshot) => {
+  snapshot.forEach((childSnapshot) => {
+      const linkData = childSnapshot.val();
+      const linkType = linkData.linkType;
+      const linkName = linkData.linkName;
+      const linkURL = linkData.linkURL;
+
+      // Create a new row div element
+      const row = document.createElement("div");
+      row.className = "data-row";
+
+      // Create and populate icon element
+      const iconElement = document.createElement("i");
+      iconElement.className = "icon fa fa-link"; // Font Awesome icon
+      row.appendChild(iconElement); // Append icon to the row
+
+      // Create and populate link element
+      const linkElement = document.createElement("a");
+      linkElement.className = "link-element";
+      linkElement.textContent = linkName;
+      linkElement.href = linkURL;
+      linkElement.target = "_blank"; // Open link in a new tab
+
+      // Append the link element to the row
+      row.appendChild(linkElement);
+
+      // Get the appropriate data section based on linkType
+      const dataSection = document.querySelector(`.${linkType} .data-section`);
+      if (dataSection) {
+          // Append the row to the data section
+          dataSection.appendChild(row);
+      }
+  });
+});
---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/*=============== scroll reveal fade in ===============*/
+const sr = ScrollReveal({
+    origin: "bottom",
+    distance: "50px",
+    duration: 1500,
+    delay: 500,
+    // reset: true,
+});
+
